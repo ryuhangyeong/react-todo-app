3a09f8d373529f307ee703506c6816befe98c6b6
ryuhangyeong <hangyeong.ryu@gmail.com>
2020-12-18 11:02:29 +0900

    todo 프로젝트 설정 및 완성

---
+++ src/components/TodoInsert.js
@@ -0,0 +1,36 @@
+import React, { useState, useCallback } from 'react';
+import { MdAdd } from 'react-icons/md';
+import './TodoInsert.scss';
+
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  // 한번 함수를 만들고 재사용하기 위해, 이 작업을 하지 않으면 매번 리렌더링될 때마다 새롭게 함수가 만들어진다.
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue('');
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
+  return (
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
+      <button type="submit">
+        <MdAdd />
+      </button>
+    </form>
+  );
+};
+
+export default TodoInsert;
